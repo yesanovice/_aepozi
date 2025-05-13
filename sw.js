e387000f81532f533e00ac54feb9ab93c01d2935
yesanovice <arkokazi08@gmail.com>
2025-05-13 17:04:42 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'aepozi-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.htm',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/images/icon-192.png',
+  '/assets/images/icon-512.png'
+  // Add other critical assets you want cached
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
